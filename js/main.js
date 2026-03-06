49eaf3f132fe3394223f5099485a50303afc2cc5
asa9i-4nonom3 <dawnpink.4nonom3@gmail.com>
2026-03-06 22:10:00 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 'use strict';
 
+$('.slide-show').slick({
+  autoplay: true, //自動再生
+  autoplaySpeed: 2500, //スライドの表示速度2.5秒
+  arrows: false, //矢印非表示
+  dots: true, //インジゲーター表示
+  fade: true,
 
-$(function(){
-  $('.btn-trigger').on('click', function() {
-    $(this).toggleClass('active');
-    return false;
-  });
 });
